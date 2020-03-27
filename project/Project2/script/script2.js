1437f301c477b4ad0e3a7b04f52c35096b933870
SHYAKA Samuel <slinbriel2019@gmail.com>
2020-03-27 12:06:12 -0700

    first commit

---
+++ project/Project2/script/script2.js
@@ -0,0 +1,24 @@
+var myIndex = 0;
+carousel();
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("switch");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 10000); // Change image every 10 seconds
+}
+// section for personal review 
+const slideshowImages = document.querySelectorAll(".review-slideshow li");
+const nextImageDelay = 10000;
+let currentImageCounter = 0; 
+slideshowImages[currentImageCounter].style.opacity = 1;
+setInterval(nextImage, nextImageDelay);
+function nextImage() {
+  slideshowImages[currentImageCounter].style.opacity = 0;
+  currentImageCounter = (currentImageCounter+1) % slideshowImages.length;
+  slideshowImages[currentImageCounter].style.opacity = 1;
+}
